131a5d7e55f7a06c779ffd83fa31ca8ddd339e5c
Afif Uddin <afifuddin@192.168.1.4>
2023-10-12 23:44:56 +0700

    [UPDATE] Drawer Nav

--- App.js
+++ App.js
@@ -5,10 +5,13 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { DetailProduct, Home, Products, Profile } from './pages'
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { createDrawerNavigator } from '@react-navigation/drawer';
 import { MyTabBar } from './components'
+import Ionicons from "@expo/vector-icons/Ionicons"
 
 const Stack = createNativeStackNavigator();
 const Tab = createBottomTabNavigator();
+const Drawer = createDrawerNavigator();
 
 function MainTab() {
   return (
@@ -52,13 +55,42 @@ function MainTab() {
   )
 }
 
+function MainDrawer() {
+  return (
+    <Drawer.Navigator
+      initialRouteName="Home"
+      screenOptions={({ route }) => ({
+        drawerActiveBackgroundColor: '#F0EDF7',
+        drawerActiveTintColor: '#6B52AD',
+        drawerIcon: ({ focused, color, size }) => {
+          let iconName;
+
+          if (route.name === "Home") {
+            iconName = "home"
+          } else if (route.name === "Products") {
+            iconName = "cart"
+          } else {
+            iconName = "person"
+          }
+
+          return <Ionicons name={iconName} size={size} color={color} />
+        },
+      })}
+    >
+      <Drawer.Screen name="Home" component={Home} />
+      <Drawer.Screen name="Products" component={Products} />
+      <Drawer.Screen name="Profile" component={Profile} />
+    </Drawer.Navigator>
+  )
+}
+
 function App() {
   return (
     <NavigationContainer>
       <Stack.Navigator>
         <Stack.Screen
-          name='MainTab'
-          component={MainTab}
+          name='MainDrawer'
+          component={MainDrawer}
           options={{ headerShown: false }}
         />
         <Stack.Screen
