1b73395cb3c130a7e8ef7951c42feda4f83b7a64
Afif Uddin <afifuddin@192.168.1.4>
2023-10-13 01:32:52 +0700

    [UPDATE] Top Tab

--- pages/Home/index.js
+++ pages/Home/index.js
@@ -1,12 +1,41 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import { createMaterialTopTabNavigator } from '@react-navigation/material-top-tabs';
+import { StyleSheet, View, Text } from 'react-native';
 
-export default function HomeScreen() {
+const Tab = createMaterialTopTabNavigator();
 
+function Terbaru() {
     return (
         <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-            <Text>Home Screen</Text>
+            <Text>Terbaru</Text>
         </View>
+    )
+}
+
+function Populer() {
+    return (
+        <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+            <Text>Populer</Text>
+        </View>
+    )
+}
+
+export default function Home() {
+    return (
+        <Tab.Navigator
+            screenOptions={{
+                tabBarActiveTintColor: "#6C53AD",
+                tabBarInactiveTintColor: 'gray',
+                tabBarLabelStyle: {
+                    fontWeight: 'bold'
+                },
+                tabBarIndicatorStyle: {
+                    backgroundColor: '#6C53AD'
+                }
+            }}
+        >
+            <Tab.Screen name="Terbaru" component={Terbaru} />
+            <Tab.Screen name="Populer" component={Populer} />
+        </Tab.Navigator>
     );
 }
 
